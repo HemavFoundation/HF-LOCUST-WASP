e365c8acb8c4781e33e31d6b8477bcf342ac642d
Adria <adri.navarrete.09@gmail.com>
2020-01-02 17:06:20 +0100

    added Cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const ___dirname = path.resolve();
 
@@ -13,6 +14,7 @@ app.set('json spaces',2);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
 // routes
 app.use("/api/connect", require("./routes/connect"));
