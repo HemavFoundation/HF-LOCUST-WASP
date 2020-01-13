9d678c6649b384254255b8ba2094c752e1460e08
SagredoIsmael <ish581@gmail.com>
2020-01-13 20:28:16 +0100

    new version results working

--- public/results/GetResults.js
+++ public/results/GetResults.js
@@ -6,25 +6,48 @@ function GetResults(){
     xhttp.open('GET','results.json',true);
     xhttp.send();
     xhttp.responseType = "text";
-
     xhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200){
             let data = JSON.parse(this.responseText);
             console.log(data);
             var i = 0;
 
-            let Results = document.querySelector("#History");
+            let Results = document.querySelector("#tableID");
             for (let item of data.flights){
                 "<tr>"
                 Results.innerHTML+="<td>"+item.id+"</td>"+"<td><button id="+i+" onclick='OpenFR(this.id)' button>GO</td>";
                 "</tr>"
                 i++;
             }  
+        }else{
+            console.log('error, readyState is:', this.readyState, 'status fetch is:', this.status)
         }
     }
 }
 function OpenFR(i){
-    //console.log(i);
-    window.open("FlightResults.html","_self");
-    ThisResult(i);
+    console.log('id is:',i);
+    ThisResult(i);  
 }
+
+function ThisResult(i){
+    console.log(i);
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET','results.json',true);
+    xhttp.send();
+    xhttp.responseType = "text";
+
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            let data = JSON.parse(this.responseText);
+            let Result = document.querySelector("#flight");         
+            
+            for (let item of data.flights[i].results){
+                "<tr>"
+                Result.innerHTML+="<td>"+item.id+"</td>"+"<td>"+item.NDVI+"</td>"+"<td>"+item.Latitude+"</td>"+"<td>"+item.Longitude+"</td>"+"<td>"+item.photo+"</td>";
+                "</tr>"
+            }
+        }
+    }
+}
+
+
