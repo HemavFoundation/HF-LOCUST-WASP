f71117aa409adbfda84de165453e6879a020a1d8
SagredoIsmael <ish581@gmail.com>
2020-01-17 17:52:16 +0100

    change index

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.listen(app.get('port'), () => {
 });
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(___dirname + '/public'));
+    res.sendFile(path.join(___dirname + '/public/planner/flightPlanner.html'));
 });
 
 app.get('/planner', (req, res) => {
