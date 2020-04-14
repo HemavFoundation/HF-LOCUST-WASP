8c884db9bfe78222ab5c09e4272f37b16bdca03d
adrianavarrete <adri.navarrete.09@gmail.com>
2020-04-14 14:43:11 +0200

    Solved  a problem with the compatibility of async await in the FAO's tablet

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
-    environment: 'win' //  'drone' for testing in the raspi / 'mac' for testing in local with macOs / 'win' for testing in local with Windows
+    environment: 'drone' //  'drone' for testing in the raspi / 'mac' for testing in local with macOs / 'win' for testing in local with Windows
 
 });
