9592ead2b9adc0cc057b01cfd560949aad142985
adrianavarrete <adri.navarrete.09@gmail.com>
2020-05-04 13:07:33 +0200

    added global variable typeOfMission

--- scripts/config.py
+++ scripts/config.py
@@ -1,3 +1,5 @@
 global connectionString
+global typeOfMission
 
 connectionString = "drone" # 'local' for testing in localhost // 'drone' for testing in the raspi
+typeOfMission = "periscope" # periscope, straight, rectangle, zigzag
