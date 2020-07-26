22680d9a88540ce9137540ec763e62c0c10bab25
adrianavarrete <adri.navarrete.09@gmail.com>
2020-07-26 11:38:12 +0200

    commit for testing on the drone

--- src/routes/start.js
+++ src/routes/start.js
@@ -5,6 +5,46 @@ const router = Router();
 const Config  = require('./config');
 const environment = Config.environment;
 
+router.get("/arm", (req, res) => {
+
+  let options;
+
+  if (environment == "drone") {
+    options = {
+      mode: "text",
+      pythonPath: "/usr/bin/python3",
+      pythonOptions: ["-u"], // get print results in real-time
+      scriptPath: "./scripts"
+    };
+  } else if (environment == "win") {
+    options = {
+      mode: "text",
+      pythonOptions: ["-u"], // get print results in real-time
+      scriptPath: "./scripts"
+    };
+  } else {
+    options = {
+      mode: "text",
+      pythonPath: "/usr/local/bin/python",
+      pythonOptions: ["-u"], // get print results in real-time
+      scriptPath: "./scripts"
+    };
+  }
+
+
+  PythonShell.run("arm.py", options, function(err, results) {
+    //if (err) throw err;
+    if (err) {
+      res.status(400).send({ message: "ERROR: Fallo el script start.py" });
+	console.log(err);
+    }else{
+    res.status(200).send({ message: "Drone armado" });
+    }
+  });
+
+});
+
+
 router.get("/", (req, res) => {
 
   let options;
@@ -38,10 +78,11 @@ router.get("/", (req, res) => {
       res.status(400).send({ message: "ERROR: Fallo el script start.py" });
 	console.log(err);
     }else{
-    res.status(200).send({ message: "Iniciado el vuelo, nos vemos a la vuelta" });
+    res.status(200).send({ message: "Proceso de captura de imagenes iniciado" });
     }
   });
 
 });
+
 
 module.exports = router;
