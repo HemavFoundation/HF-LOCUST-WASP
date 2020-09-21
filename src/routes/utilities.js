8234bee8ea54e0dfb037eee846c04d04aa20ac18
Adrià Navarrete <anavarrete@hemav.com>
2020-09-21 18:18:07 +0200

    First implementation for the disk space

---
+++ src/routes/utilities.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const checkDiskSpace = require('check-disk-space');
+const router = Router();
+
+const Config = require("./config");
+const environment = Config.environment
+
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+router.get('/SDdiskSpace', (req, res) => {
+
+    if (environment == 'drone' || environment == 'mac') {
+        checkDiskSpace('').then((disk) => {
+            res.status(200).send({
+                freeSpace: round(disk.free / 1073741824, 2),
+                SizeSpace: round(disk.size / 1073741824, 2)
+            })
+        })
+    } else {
+        checkDiskSpace('C:/').then((disk) => {
+            res.status(200).send({
+                freeSpace: round(disk.free / 1073741824, 2),
+                SizeSpace: round(disk.size / 1073741824, 2)
+            })
+
+        })
+    }
+
+})
+
+module.exports = router;
